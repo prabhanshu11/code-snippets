08f7eb77ee9f5744cd410b3b936a9bd1b82a26d4
prabhanshu11 <mail.prabhanshu@gmail.com>
2021-11-20 17:43:51 +0530

    Update value_counts.py

--- value_counts.py
+++ value_counts.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
-def value_count(s :pd.Series):
-	
+def value_count(s: pd.Series):
+	"""
+	``add docs later``
+	"""
+	name = s.name
+	VC = s.value_counts().reset_index().rename(columns={
+		'index' : name,
+		name : 'count'})
+	VC['norm'] = VC['count'] / VC['count'].sum()
+	return VC
