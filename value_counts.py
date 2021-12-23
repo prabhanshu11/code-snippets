c42abe14ba5eb38ee84886d59968d875929a69d8
prabhanshu11 <mail.prabhanshu@gmail.com>
2021-12-23 14:01:22 +0530

    added null values count

--- value_counts.py
+++ value_counts.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
 def value_count(s: pd.Series):
-	"""
-	``add docs later``
-	"""
-	name = s.name
-	VC = s.value_counts().reset_index().rename(columns={
-		'index' : name,
-		name : 'count'})
-	VC['norm'] = VC['count'] / VC['count'].sum()
-	return VC
+    """
+    ``add docs later``
+    """
+    name = s.name
+    VC = s.value_counts().reset_index().rename(columns={
+        'index' : name,
+        name : 'count'})
+    VC = pd.concat([
+        VC,
+        pd.DataFrame(
+            data=[['--', s.isna().sum()]], 
+            index=['NaNs'], 
+            columns=[name, 'count']
+        )])
+    VC['norm'] = VC['count'] / VC['count'].sum()
+    return VC
