a0f1ccaf380f253f14991e9a41a56943f0876f31
prabhanshu11 <mail.prabhanshu@gmail.com>
2022-03-24 13:23:31 +0530

    Now it can print "Total".
    The feature is available as a flag in the function.

--- value_counts.py
+++ value_counts.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-def value_count(s: pd.Series):
-    """
-    ``add docs later``
-    """
+def value_count(s: pd.Series, print_total=False):
     name = s.name
     VC = s.value_counts().reset_index().rename(columns={
         'index' : name,
@@ -15,5 +12,19 @@ def value_count(s: pd.Series):
             index=['NaNs'], 
             columns=[name, 'count']
         )])
+     
     VC['norm'] = VC['count'] / VC['count'].sum()
+    
+    if print_total == True:
+        VC = pd.concat([
+            VC,
+            pd.DataFrame( 
+                index=['--', 
+                       'TOTAL'
+                      ], 
+                columns=[name, 'count', 'norm'],
+                data=[['--',              '--',              '--'],
+                      ['--', VC['count'].sum(),  VC['norm'].sum()]],              
+            )
+        ])
     return VC
